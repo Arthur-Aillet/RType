383af8c4d8bef26c704e65496fe36cee560bfd6a
Arthur-Aillet <arthur.aillet@epitech.eu>
2023-12-11 13:36:27 +0100

    feat: stages and system exemples, main adapted for Cevy update

--- src/main.cpp
+++ src/main.cpp
@@ -1,11 +1,15 @@
 #include <iostream>
 
-#include "../Cevy/src/registry.hpp"
+#include "../Cevy/src/cevy.hpp"
+#include "../Cevy/src/ecs/ecs.hpp"
+#include "../Cevy/src/ecs/App.hpp"
+#include "../Cevy/src/ecs/Plugin.hpp"
+#include "../Cevy/src/ecs/Schedule.hpp"
 #include "../Cevy/src/game_engine/game_engine.hpp"
 #include "../Cevy/src/game_engine/position.hpp"
 #include "../Cevy/src/game_engine/velocity.hpp"
 
-void logging_system(registry &r,
+void logging_system(cevy::ecs::World &w,
     sparse_array<position> const &positions,
     sparse_array<velocity> const &velocities) {
     for (size_t i = 0; i < positions.size() && i < velocities.size(); ++i) {
@@ -18,18 +22,60 @@ void logging_system(registry &r,
     }
 }
 
+void update(cevy::ecs::World &w) {
+    std::cerr << "Update" << std::endl;
+}
+
+void post_update(cevy::ecs::World &w) {
+    std::cerr << "Post Update" << std::endl;
+}
+
+void pre_update(cevy::ecs::World &w) {
+    std::cerr << "Pre Update" << std::endl;
+}
+
+void startup(cevy::ecs::World &w) {
+    std::cerr << "Startup" << std::endl;
+}
+
+void pre_startup(cevy::ecs::World &w) {
+    std::cerr << "pre Startup" << std::endl;
+}
+
+class GoodByePlugin : public cevy::ecs::Plugin {
+    public: void build(cevy::ecs::App& app) override
+    {
+        app.register_component<float>();
+    }
+};
+
+
+class HelloPlugin : public cevy::ecs::Plugin {
+    public: void build(cevy::ecs::App& app) override
+    {
+        app.register_component<int>();
+    }
+};
+
 int main() {
-    registry app = registry();
+    cevy::ecs::App app;
+    app.add_plugins<HelloPlugin>();
     app.register_component<position>();
     app.register_component<velocity>();
     app.register_component<drawable>();
     app.register_component<controllable>();
-    entity movable = app.spawn_entity();
+    cevy::ecs::Entity movable = app.spawn_empty();
     app.add_component(movable, position {0, 0});
     app.add_component(movable, velocity {3, 1});
-    app.add_system<position, velocity>(logging_system);
+
+    app.add_system<cevy::ecs::Schedule::Update>(update);
+    app.add_system<cevy::ecs::Schedule::PreUpdate>(pre_update);
+    app.add_system<cevy::ecs::Schedule::PostUpdate>(post_update);
+    app.add_system<cevy::ecs::Schedule::Startup>(startup);
+    app.add_system<cevy::ecs::Schedule::PreStartup>(pre_startup);
+//    app.add_system<position, velocity>(logging_system);
     std::cout << "Hello world!" << std::endl;
-    app.run_systems();
+    app.run();
     std::cout << "Hello world!" << std::endl;
     return 0;
 }
