e7a17c4117415b0612a8c82648479b2d17986d73
Arthur-Aillet <arthur.aillet@epitech.eu>
2024-01-13 18:49:10 +0100

    feat: used clear color and new Transform

--- src/main.cpp
+++ src/main.cpp
@@ -2,11 +2,11 @@
 #include "Asset.hpp"
 #include "AssetManager.hpp"
 #include "Camera.hpp"
+#include "ClearColor.hpp"
 #include "Color.hpp"
 #include "Commands.hpp"
 #include "EntityCommands.hpp"
 #include "Line.hpp"
-#include "Position.hpp"
 #include "Query.hpp"
 #include "Target.hpp"
 #include "Time.hpp"
@@ -15,6 +15,7 @@
 #include "ecs.hpp"
 #include "engine/Engine.hpp"
 #include "input.hpp"
+#include "raylib.h"
 #include "raylib.hpp"
 #include <raymath.h>
 
@@ -32,56 +33,50 @@ void create_player_ship(Resource<Asset<cevy::engine::Mesh>> meshs, Resource<Asse
   auto handle_mesh = meshs.get().load("assets/space-ship1.obj");
   // handle_mesh.get().mesh.transform = MatrixRotateXYZ({0, M_PI, 0});
 
-  cmd.spawn(cevy::engine::Transform(), handle_mesh, handle_difs, PlayerMarker(), Position());
+  cmd.spawn(cevy::engine::Transform(), handle_mesh, handle_difs, PlayerMarker());
 }
 
 void control_spaceship(Resource<Time> time, Query<Line> lines,
-                       Query<cevy::engine::Camera, Target> cams,
+                       Query<cevy::engine::Camera, cevy::engine::Transform> cams,
                        Query<PlayerMarker, cevy::engine::Transform> spaceship) {
   for (auto [space, tm] : spaceship) {
-    float delta = 1 * time.get().delta_seconds();
+    float delta = 90 * time.get().delta_seconds() * DEG2RAD;
+
     if (cevy::Keyboard::keyDown(KEY_W)) {
-      tm.rotateXYZ({delta, 0, 0});
+      tm.rotateX(delta);
     }
     if (cevy::Keyboard::keyDown(KEY_S)) {
-      tm.rotateXYZ({-delta, 0, 0});
+      tm.rotateX(-delta);
     }
     if (cevy::Keyboard::keyDown(KEY_A)) {
-      tm.rotateXYZ({0, delta, 0});
+      tm.rotateY(delta);
     }
     if (cevy::Keyboard::keyDown(KEY_D)) {
-      tm.rotateXYZ({0, -delta, 0});
+      tm.rotateY(-delta);
     }
     if (cevy::Keyboard::keyDown(KEY_E)) {
-      tm.rotateXYZ({0, 0, delta});
+      tm.rotateZ(delta);
     }
     if (cevy::Keyboard::keyDown(KEY_Q)) {
-      tm.rotateXYZ({0, 0, -delta});
+      tm.rotateZ(-delta);
     }
+    auto [cam, cam_tranform] = cams.single();
 
-    auto [one, two, three] = lines.multiple<3>();
-    auto [fst] = one;
-    auto [snd] = two;
-    auto [trd] = three;
+    Vector fwd = cevy::engine::Vector(0, 0, 1);
+    fwd.rotate(tm.rotation);
+    static float speed = 0;
+    speed += 0.0005;
+    tm.position += fwd * (delta + speed);
 
-    Vector new_u = Position(5, 0, 0);
-    Vector new_v = Position(0, 5, 0);
-    Vector new_w = Position(0, 0, 5);
-    new_u = tm * new_u;
-    new_v = tm * new_v;
-    new_w = tm * new_w;
-    fst.end = new_u;
-    snd.end = new_v;
-    trd.end = new_w;
-
-    // for (auto [cam, target, c_pos] : cams) {
-    //   Vector camera_target = pos + Vector(0, 5, 5);
-    //   camera_target.rotate(tm * DEG2RAD);
-    //   target = camera_target;
-    //   Vector camera_pos = pos + Vector(0, 10, -10);
-    //   c_pos = camera_pos;
-    // }
+    cam_tranform.rotation = tm.rotation;
+    cam_tranform.position = cevy::engine::Vector(0, 3, -10);
+    cam_tranform.position.rotate(tm.rotation);
+    cam_tranform.position += tm.position;
   }
+}
+
+void set_background(Resource<ClearColor> col) {
+  col.get() = ClearColor(cevy::engine::Color(0, 0, 0));
 }
 
 int main() {
@@ -91,18 +86,9 @@ int main() {
   app.insert_resource(AssetManager());
   app.add_plugins(Engine());
   app.add_systems<core_stage::Startup>(create_player_ship);
+  app.add_systems<core_stage::Startup>(set_background);
   app.add_systems<core_stage::Update>(control_spaceship);
-  app.spawn(Line(Position(0, 0, 0), Position(5, 0, 0)), cevy::engine::Color(255, 200, 0));
-  app.spawn(Line(Position(0, 0, 0), Position(0, 5, 0)), cevy::engine::Color(0, 255, 200));
-  app.spawn(Line(Position(0, 0, 0), Position(0, 0, 5)), cevy::engine::Color(0, 200, 255));
-  app.spawn(Line(Position(0, 0, -5), Position(0, 4, -5)), cevy::engine::Color(255, 255, 0));
-  app.spawn(Line(Position(0, 0, +5), Position(0, 3, +5)), cevy::engine::Color(255, 0, 255));
-
-  app.spawn(Line(Position(0, 0, 0), Position(4, 0, 0)), cevy::engine::Color(255, 0, 0, 50));
-  app.spawn(Line(Position(0, 0, 0), Position(0, 4, 0)), cevy::engine::Color(0, 255, 0, 50));
-  app.spawn(Line(Position(0, 0, 0), Position(0, 0, 4)), cevy::engine::Color(0, 0, 255, 50));
-
-  app.spawn(cevy::engine::Camera(), cevy::engine::Position(10.0, 10.0, 10), cevy::engine::Target(0.0, 0.0, 0.0));
+  app.spawn(cevy::engine::Camera(), cevy::engine::Transform(Vector(0, 5, -10)));
   app.run();
   return 0;
 }
