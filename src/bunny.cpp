931916e14ef2fd056bd79a1b18577e8ec58f80a7
Arthur-Aillet <arthur.aillet@epitech.eu>
2024-12-24 03:15:14 +0900

    feat: updated bunny exemple to display upgrades to  cevy

--- src/bunny.cpp
+++ src/bunny.cpp
@@ -1,8 +1,10 @@
 #include "Asset.hpp"
 #include "AssetManager.hpp"
+#include "Color.hpp"
+#include "PbrMaterial.hpp"
+#include "commands/EntityCommands.hpp"
 #include "engine/Engine.hpp"
 #include "commands/Commands.hpp"
-#include "commands/EntityCommands.hpp"
 #include "engine/Transform.hpp"
 #include "engine/Camera.hpp"
 #include "ecs/App.hpp"
@@ -14,11 +16,14 @@ using namespace engine;
 
 float DEG2RAD = glm::pi<float>() / 180;
 
-int initial_setup(Resource<Asset<cevy::engine::Model>> mesh_manager, Resource<Asset<Diffuse>> difs,
+int initial_setup(Resource<Asset<cevy::engine::Model>> mesh_manager, Resource<Asset<cevy::engine::PbrMaterial>> material_manager,
                    Commands cmd, World &w) {
   auto& meshs = mesh_manager.get();
+  auto &mats = material_manager.get();
   auto bunny_handle = meshs.load(Bunny::model());
-  cmd.spawn(Transform(), bunny_handle);
+  auto mat_h = mats.load(PbrMaterial(glm::vec3(0., 1., 1.), glm::vec3(1), 12));
+
+  cmd.spawn(Transform(), bunny_handle, mat_h, Color(0, 0, 1));
   cmd.spawn(cevy::engine::Camera(),
             cevy::engine::Transform(glm::vec3(0, -2, -10)));
   // cmd.spawn(cevy::engine::Camera(),
