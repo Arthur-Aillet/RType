433218d57b40e41033924e9dc8a427a6a04e48dc
Arthur-Aillet <arthur.aillet@epitech.eu>
2025-01-03 03:29:49 +0900

    feat: added multiple light

--- src/bunny.cpp
+++ src/bunny.cpp
@@ -1,7 +1,10 @@
+#define GLM_FORCE_SWIZZLE
+#define GLM_ENABLE_EXPERIMENTAL
 #include "Asset.hpp"
 #include "AssetManager.hpp"
 #include "Color.hpp"
 #include "PbrMaterial.hpp"
+#include "PointLight.hpp"
 #include "commands/EntityCommands.hpp"
 #include "engine/Engine.hpp"
 #include "commands/Commands.hpp"
@@ -16,6 +19,12 @@ using namespace engine;
 
 float DEG2RAD = glm::pi<float>() / 180;
 
+static glm::vec3 hsv2rgb(glm::vec3 c) {
+  glm::vec4 K = glm::vec4(1.0, 2.0 / 3.0, 1.0 / 3.0, 3.0);
+  glm::vec3 p = abs(fract(c.xxx() + K.xyz()) * 6.0f - K.www());
+  return c.z * mix(K.xxx(), clamp(p - K.xxx(), 0.0f, 1.0f), c.y);
+}
+
 int initial_setup(Resource<Asset<cevy::engine::Model>> mesh_manager, Resource<Asset<cevy::engine::PbrMaterial>> material_manager,
                    Commands cmd, World &w) {
   auto& meshs = mesh_manager.get();
@@ -26,6 +35,17 @@ int initial_setup(Resource<Asset<cevy::engine::Model>> mesh_manager, Resource<As
   cmd.spawn(Transform(), bunny_handle, mat_h, Color(0, 0, 1));
   cmd.spawn(cevy::engine::Camera(),
             cevy::engine::Transform(glm::vec3(0, -2, -10)));
+
+  const int ringCount = 5;
+  const float ringRadius = 5;
+  for (int i = 0; i < ringCount; i++) {
+    glm::vec3 rgb = 20.f * hsv2rgb({float(i) / ringCount, 0.9, 1.0f});
+    Transform tm = cevy::engine::Transform(glm::vec3(ringRadius * std::cos(glm::two_pi<float>() * float(i) / ringCount)));
+    //, 3.0f,
+    //                  ringRadius * std::sin(glm::two_pi<float>() * float(i) / ringCount)), glm::quat({0, 0, 0}), glm::vec3(.5, .5, .5));
+    PointLight light = {rgb, 1.0f};
+    auto entity = cmd.spawn(tm, light, bunny_handle, mat_h);
+  }
   // cmd.spawn(cevy::engine::Camera(),
   //           cevy::engine::Transform());
 
